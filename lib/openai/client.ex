fa6e33c15509de5895e2d4a39ba030cf0e49642f
Feynman Liang <feynman.liang@gmail.com>
2023-04-22 17:45:05 +0000

    Use Tesla.request

--- lib/openai/client.ex
+++ lib/openai/client.ex
@@ -4,6 +4,7 @@ defmodule OpenAI.Client do
     middleware = [
       {Tesla.Middleware.BaseUrl, "https://fmops.ai/api/v1/proxy/openai/v1"},
       {Tesla.Middleware.Headers, [
+          # TODO: dynamic client config with per-tenant key from opts
          {"Authorization", "Bearer #{System.get_env("OPENAI_API_KEY")}"}
       ]},
       Tesla.Middleware.JSON
@@ -11,18 +12,10 @@ defmodule OpenAI.Client do
 
     tesla = Tesla.client(middleware)
 
-    # TODO: dynamic client config with per-tenant key from opts
-    case data.method do
-      :get ->
-        tesla
-        |> Tesla.get(data.url)
-
-      :post ->
-        tesla
-        |> Tesla.post(
-          data.url,
-          data.body
-        )
-    end
+    tesla
+    |> Tesla.request(
+      method: data.method,
+      url: data.url,
+      body: data.body)
   end
 end
