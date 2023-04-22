43ed1e1bee3a9ae83c02c6b6041b9c756f0438f5
Feynman Liang <feynman.liang@gmail.com>
2023-04-22 17:58:14 +0000

    Load openai from config

--- lib/openai/client.ex
+++ lib/openai/client.ex
@@ -1,13 +1,20 @@
 defmodule OpenAI.Client do
+  @base_middleware [
+    {Tesla.Middleware.BaseUrl, Application.compile_env(
+      :oapi_oai,
+      :openai_base_url,
+      "https://api.openai.com/v1"
+    )},
+    Tesla.Middleware.JSON
+  ]
 
-  def request(data, _opts \\ nil) do
+  def request(data) do
+    api_key = Keyword.get(data.opts, :openai_api_key)
     middleware = [
-      {Tesla.Middleware.BaseUrl, "https://fmops.ai/api/v1/proxy/openai/v1"},
       {Tesla.Middleware.Headers, [
-          # TODO: dynamic client config with per-tenant key from opts
-         {"Authorization", "Bearer #{System.get_env("OPENAI_API_KEY")}"}
-      ]},
-      Tesla.Middleware.JSON
+         {"Authorization", "Bearer #{api_key}"}
+      ]}
+      | @base_middleware
     ]
 
     tesla = Tesla.client(middleware)
