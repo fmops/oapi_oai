35fba6dd4c7369a766a2c850b170cc78b9fba645
Feynman Liang <feynman.liang@gmail.com>
2023-05-01 22:14:39 +0000

    Adds chat completions test

--- test/open_ai_test.exs
+++ test/open_ai_test.exs
@@ -7,36 +7,76 @@ defmodule OpenAITest do
     {:ok, bypass: bypass}
   end
 
-  test "fails with missing API key", %{bypass: bypass} do
-    Bypass.expect_once(bypass, fn conn ->
-      Stubidity.call(conn, [])
-    end)
+  describe "completions" do
+    test "fails with missing API key", %{bypass: bypass} do
+      Bypass.expect_once(bypass, fn conn ->
+        Stubidity.call(conn, [])
+      end)
 
-    assert :error =
-             OpenAI.OpenAI.create_completion(
-               %{
-                 prompt: "Once upon a time",
-                 model: "davinci"
-               },
-               base_url: "http://localhost:#{bypass.port}/v1"
-             )
+      assert :error =
+              OpenAI.OpenAI.create_completion(
+                %{
+                  prompt: "Once upon a time",
+                  model: "davinci"
+                },
+                base_url: "http://localhost:#{bypass.port}/v1"
+              )
+    end
+
+    test "suceeds with valid API key", %{bypass: bypass} do
+      Bypass.expect_once(bypass, fn conn ->
+        Stubidity.call(conn, [])
+      end)
+
+      assert {:ok, resp} =
+              OpenAI.OpenAI.create_completion(
+                %{
+                  prompt: "Once upon a time",
+                  model: "davinci"
+                },
+                openai_api_key: "sk-foo",
+                base_url: "http://localhost:#{bypass.port}/v1"
+              )
+
+      assert Enum.at(resp.choices, 0)["text"] =~ "I am writing"
+    end
   end
 
-  test "suceeds with valid API key", %{bypass: bypass} do
-    Bypass.expect_once(bypass, fn conn ->
-      Stubidity.call(conn, [])
-    end)
+  describe "chat completions" do
+    test "fails with missing API key", %{bypass: bypass} do
+      Bypass.expect_once(bypass, fn conn ->
+        Stubidity.call(conn, [])
+      end)
 
-    assert {:ok, resp} =
-             OpenAI.OpenAI.create_completion(
-               %{
-                 prompt: "Once upon a time",
-                 model: "davinci"
-               },
-               openai_api_key: System.get_env("OPENAI_API_KEY"),
-               base_url: "http://localhost:#{bypass.port}/v1"
-             )
+      assert :error =
+              OpenAI.OpenAI.create_chat_completion(
+                %{
+                  model: "gpt-3.5-turbo",
+                  messages: [
+                    %{role: "system", text: "Tell a joke."}
+                  ]
+                },
+                base_url: "http://localhost:#{bypass.port}/v1"
+              )
+    end
 
-    assert Enum.at(resp.choices, 0)["text"] =~ "I am writing"
+    test "suceeds with valid API key", %{bypass: bypass} do
+      Bypass.expect_once(bypass, fn conn ->
+        Stubidity.call(conn, [])
+      end)
+
+      assert {:ok, resp} =
+              OpenAI.OpenAI.create_chat_completion(
+                %{
+                  model: "gpt-3.5-turbo",
+                  messages: [
+                    %{role: "system", text: "Tell a joke."}
+                  ]
+                },
+                openai_api_key: "sk-foo",
+                base_url: "http://localhost:#{bypass.port}/v1"
+              )
+      assert Enum.at(resp.choices, 0)["message"]["role"] == "assistant"
+    end
   end
 end
