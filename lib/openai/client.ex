ac00d1643f9d33ddcbae100c102490470a6fa7c7
Feynman Liang <feynman.liang@gmail.com>
2023-05-01 21:54:11 +0000

    Adds invalid api key test

--- lib/openai/client.ex
+++ lib/openai/client.ex
@@ -1,21 +1,38 @@
 defmodule OpenAI.Client do
+  require Logger
+
   @base_middleware [
     {Tesla.Middleware.BaseUrl, Application.compile_env(
       :oapi_oai,
       :openai_base_url,
       "https://api.openai.com/v1"
     )},
+    {Tesla.Middleware.Headers,
+     [
+       {"content-type", "application/json"}
+     ]},
     Tesla.Middleware.JSON
   ]
 
   def request(data) do
     api_key = Keyword.get(data.opts, :openai_api_key)
-    middleware = [
-      {Tesla.Middleware.Headers, [
-         {"Authorization", "Bearer #{api_key}"}
-      ]}
-      | @base_middleware
-    ]
+    base_url = Keyword.get(data.opts, :base_url, "https://api.openai.com/v1")
+
+    middleware =
+      if api_key != nil do
+        [
+          {Tesla.Middleware.Headers,
+           [
+             {"Authorization", "Bearer #{api_key}"}
+           ]}
+          | @base_middleware
+        ]
+      else
+        @base_middleware
+      end
+      |> then(fn m ->
+        [{Tesla.Middleware.BaseUrl, base_url} | m]
+      end)
 
     tesla = Tesla.client(middleware)
 
--- test/open_ai_test.exs
+++ test/open_ai_test.exs
@@ -2,7 +2,14 @@ defmodule OpenAITest do
   use ExUnit.Case
   doctest OpenAI
 
-  test "completions" do
+  test "fails with missing API key" do
+    assert :error = OpenAI.OpenAI.create_completion(%{
+      prompt: "Once upon a time",
+      model: "davinci"
+    }, openai_api_key: "")
+  end
+
+  test "suceeds with valid API key" do
     assert {:ok, _resp} = OpenAI.OpenAI.create_completion(%{
       prompt: "Once upon a time",
       model: "davinci"
