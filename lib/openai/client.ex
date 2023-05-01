b04a4ec0c4b1191f3137dd017d041a5c354016a8
Feynman Liang <feynman.liang@gmail.com>
2023-05-01 22:10:57 +0000

    Assert on response text

--- lib/openai/client.ex
+++ lib/openai/client.ex
@@ -2,11 +2,12 @@ defmodule OpenAI.Client do
   require Logger
 
   @base_middleware [
-    {Tesla.Middleware.BaseUrl, Application.compile_env(
-      :oapi_oai,
-      :openai_base_url,
-      "https://api.openai.com/v1"
-    )},
+    {Tesla.Middleware.BaseUrl,
+     Application.compile_env(
+       :oapi_oai,
+       :openai_base_url,
+       "https://api.openai.com/v1"
+     )},
     Tesla.Middleware.JSON
   ]
 
@@ -36,17 +37,22 @@ defmodule OpenAI.Client do
     |> Tesla.request(
       method: data.method,
       url: data.url,
-      body: data.body)
+      body: data.body
+    )
     |> case do
       {:ok, %Tesla.Env{status: 200, body: body} = resp} ->
         struct = struct(data.response |> Enum.at(0) |> elem(1) |> elem(0))
-        resp = Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
-          case Map.fetch(body, Atom.to_string(k)) do
-            {:ok, v} -> %{acc | k => v}
-            :error -> acc
-          end
-        end
+
+        resp =
+          Enum.reduce(Map.to_list(struct), struct, fn {k, _}, acc ->
+            case Map.fetch(body, Atom.to_string(k)) do
+              {:ok, v} -> %{acc | k => v}
+              :error -> acc
+            end
+          end)
+
         {:ok, resp}
+
       error ->
         Logger.warn("Error: #{inspect(error)}")
         :error
--- test/open_ai_test.exs
+++ test/open_ai_test.exs
@@ -12,10 +12,14 @@ defmodule OpenAITest do
       Stubidity.call(conn, [])
     end)
 
-    assert :error = OpenAI.OpenAI.create_completion(%{
-      prompt: "Once upon a time",
-      model: "davinci"
-    }, base_url: "http://localhost:#{bypass.port}/v1")
+    assert :error =
+             OpenAI.OpenAI.create_completion(
+               %{
+                 prompt: "Once upon a time",
+                 model: "davinci"
+               },
+               base_url: "http://localhost:#{bypass.port}/v1"
+             )
   end
 
   test "suceeds with valid API key", %{bypass: bypass} do
@@ -23,9 +27,16 @@ defmodule OpenAITest do
       Stubidity.call(conn, [])
     end)
 
-    assert {:ok, _resp} = OpenAI.OpenAI.create_completion(%{
-      prompt: "Once upon a time",
-      model: "davinci"
-    }, openai_api_key: System.get_env("OPENAI_API_KEY"), base_url: "http://localhost:#{bypass.port}/v1")
+    assert {:ok, resp} =
+             OpenAI.OpenAI.create_completion(
+               %{
+                 prompt: "Once upon a time",
+                 model: "davinci"
+               },
+               openai_api_key: System.get_env("OPENAI_API_KEY"),
+               base_url: "http://localhost:#{bypass.port}/v1"
+             )
+
+    assert Enum.at(resp.choices, 0)["text"] =~ "I am writing"
   end
 end
