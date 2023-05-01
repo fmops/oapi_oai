b7c86c88a6f8a75b1f4548e13e52b27a2138d154
Feynman Liang <feynman.liang@gmail.com>
2023-05-01 21:39:34 +0000

    Dont configure oapi_generator outside dev

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,12 @@
 import Config
 
-config :oapi_generator,
-  default: [
-    base_location: "lib/openai",
-    base_module: OpenAI
-  ]
+if config_env() == :dev do
+  config :oapi_generator,
+    default: [
+      base_location: "lib/openai",
+      base_module: OpenAI
+    ]
+end
 
 config :tesla,
   adapter: {Tesla.Adapter.Finch, name: OpenAI.Finch}
